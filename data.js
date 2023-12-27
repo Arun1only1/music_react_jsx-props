d49182b3e936a415267c3a14fd4a33e2cf9d024c
Arun1only1 <arun1nly1@gmail.com>
2023-12-27 17:26:53 +0545

    array render

---
+++ data.js
@@ -0,0 +1,26 @@
+export const bookList = [
+  {
+    id: 1,
+    img: "https://m.media-amazon.com/images/I/811HITbWChL._SY466_.jpg",
+    author: "Alex Snodgrass",
+    name: "Dinner Tonight",
+  },
+  {
+    id: 2,
+    img: "https://m.media-amazon.com/images/I/41C-012Es8L._SY445_SX342_.jpg",
+    author: "James Clear",
+    name: "Atomic Habits",
+  },
+  {
+    id: 3,
+    img: "https://m.media-amazon.com/images/I/51sx4eLa6NL._SY445_SX342_.jpg",
+    author: "Freida McFadden",
+    name: "The Housemaid",
+  },
+  {
+    id: 4,
+    img: "https://m.media-amazon.com/images/I/81im9aAFBOL._SY466_.jpg",
+    author: "Rebecca Yarros",
+    name: "Fourth Wing",
+  },
+];
--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,20 +1,13 @@
 import React from "react";
 
 const Book = (props) => {
-  const { img, author, name } = props.bookDetails;
+  const { img, author, name } = props.book;
+
   return (
-    <div
-      style={{
-        padding: "1rem",
-        margin: "2rem",
-        borderRadius: "10px",
-        boxShadow:
-          "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
-      }}
-    >
-      <img src={img} />
-      <h3>{name}</h3>
-      <h6>{author}</h6>
+    <div>
+      <img src={img} alt={name} />
+      <h3>Name:{name}</h3>
+      <h6> Author:{author}</h6>
     </div>
   );
 };
